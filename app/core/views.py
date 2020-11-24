48d49955baaecb2a5fca9c8192c2fa291f02741f
Fernando Narbona <fernandonarbona@Fernandos-MacBook-Pro.local>
2020-11-24 13:40:17 -0500

    added first APIView

--- app/core/views.py
+++ app/core/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    "test API View"
+
+    def get(self, request, format=None):
+        "returns a list of apiview features"
+        an_apiview = [
+            'uses http methods as functions'
+            'is similar to a traditional django view',
+            'gives you the most control over app logic',
+            'is mapped manually to URLs'
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
