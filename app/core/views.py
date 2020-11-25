21098d385ff28ec88f248251b7b067c05ad0b3ae
Fernando Narbona <fernandonarbona@Fernandos-MacBook-Pro.local>
2020-11-24 21:37:36 -0500

    added authorization token

--- app/core/views.py
+++ app/core/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import status, viewsets, filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from core import serializers, models, permissions
 
@@ -95,3 +97,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile, )
     filter_backends = (filters.SearchFilter, )
     search_fields = ('name', 'email', )
+
+
+class UserLoginApiView(ObtainAuthToken):
+    "handle creating user auth tokens"
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
